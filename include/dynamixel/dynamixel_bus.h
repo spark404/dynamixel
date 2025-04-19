b7e54979ca9f9e08ce83acc3c23ffc2470008b6f
Hugo Trippaers <hugo@trippaers.nl>
2025-04-19 15:26:23 +0200

    Add function to flush the bus

--- include/dynamixel/dynamixel_bus.h
+++ include/dynamixel/dynamixel_bus.h
@@ -12,10 +12,12 @@
 
 typedef ssize_t (*dynamixelWriteFunc_t) (const uint8_t *txBuffer, size_t size, void *pvArgument);
 typedef ssize_t (*dynamixelReadFunc_t) (uint8_t *rxBuffer, size_t size, void *pvArgument);
+typedef void (*dynamixelFlushFunc_t) (void *pvArgument);
 
 typedef struct dynamixel_bus {
     dynamixelWriteFunc_t writeFunc;
     dynamixelReadFunc_t readFunc;
+    dynamixelFlushFunc_t flushFunc;
     void *pvContext;
 } dynamixel_bus_t;
 
@@ -24,8 +26,9 @@ extern "C" {
 #endif
 
 dynamixel_result_t dynamixel_bus_init(dynamixel_bus_t *bus, dynamixelReadFunc_t readFunc, dynamixelWriteFunc_t writeFunc, void *pvContext);
-ssize_t dynamixel_bus_write(dynamixel_bus_t *bus, uint8_t *buffer, size_t buffer_length);
-ssize_t dynamixel_bus_read(dynamixel_bus_t *bus, uint8_t *buffer, size_t buffer_length);
+ssize_t dynamixel_bus_write(const dynamixel_bus_t *bus, uint8_t *buffer, size_t buffer_length);
+ssize_t dynamixel_bus_read(const dynamixel_bus_t *bus, uint8_t *buffer, size_t buffer_length);
+void dynamixel_bus_flush(const dynamixel_bus_t *bus);
 
 #ifdef __cplusplus
 }
--- src/dynamixel_bus.c
+++ src/dynamixel_bus.c
@@ -13,11 +13,22 @@ dynamixel_result_t dynamixel_bus_init(dynamixel_bus_t *bus, dynamixelReadFunc_t 
     return DNM_OK;
 }
 
-ssize_t dynamixel_bus_write(dynamixel_bus_t *bus, uint8_t *buffer, size_t buffer_length) {
+ssize_t dynamixel_bus_write(const dynamixel_bus_t *bus, uint8_t *buffer, size_t buffer_length) {
+    if (bus->writeFunc == NULL) {
+        return -1;
+    }
     return bus->writeFunc(buffer, buffer_length, bus->pvContext);
 }
 
-ssize_t dynamixel_bus_read(dynamixel_bus_t *bus, uint8_t *buffer, size_t buffer_length) {
+ssize_t dynamixel_bus_read(const dynamixel_bus_t *bus, uint8_t *buffer, size_t buffer_length) {
+    if (bus->readFunc == NULL) {
+        return -1;
+    }
     return bus->readFunc(buffer, buffer_length, bus->pvContext);
+}
 
+void dynamixel_bus_flush(const dynamixel_bus_t *bus) {
+    if (bus->flushFunc != NULL) {
+        bus->flushFunc(bus->pvContext);
+    }
 }
