a91220ead7f5c24ccfb39c6606f569684652151c
Hugo Trippaers <hugo@trippaers.nl>
2025-07-14 20:23:46 +0200

    Add missing initializer

--- tests/api_TEST.cc
+++ tests/api_TEST.cc
@@ -18,6 +18,7 @@ protected:
         _bus = {
             .writeFunc = &mock_write,
             .readFunc = &mock_read,
+            .flushFunc = nullptr,
             .pvContext = this
         };
     }
